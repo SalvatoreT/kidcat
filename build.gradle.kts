f0f0c0976531159092ed2666b2b9e0bff37ab2bc
Salvatore Testa <code@sal.dev>
2022-04-02 23:11:05 -0700

    Get a basic logcat example working
    Include the "Clikt" and "dadb" libraries now because I'm getting on a
    plane and want to make sure I have them downloaded.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    application
+  kotlin("jvm") version "1.6.10"
+  application
 }
 
 group = "dev.sal"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  implementation("dev.mobile:dadb:0.0.9")
+  implementation("com.github.ajalt.clikt:clikt:3.4.0")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+  kotlinOptions.jvmTarget = "11"
 }
 
 application {
-    mainClass.set("MainKt")
+  mainClass.set("Main")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package dev.sal.kidcat
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import com.github.ajalt.clikt.core.CliktCommand
+import dadb.Dadb
+
+class Main : CliktCommand() {
+  override fun run() {
+    val dadb = Dadb.discover("localhost")
+
+    if (dadb == null) {
+      println("No emulator found")
+      return
+    }
+
+    dadb.openShell("logcat").use {
+      while (true) {
+        print(it.read())
+      }
+    }
+  }
 }
+
+fun main(args: Array<String>) = Main().main(args)
