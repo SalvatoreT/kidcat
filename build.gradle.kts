6fe4b9e547e3f63a25a4757351a843874a181f6a
Salvatore Testa <code@sal.dev>
2022-07-30 17:57:43 -0700

    Update Kotlin 1.7.10, set tool version to 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.6.10"
+  kotlin("jvm") version "1.7.10"
   application
 }
 
 group = "dev.sal"
-version = "1.0"
+version = "0.1.0"
 
 repositories {
   mavenCentral()
 }
 
 dependencies {
-  implementation("dev.mobile:dadb:0.0.9")
-  implementation("com.github.ajalt.clikt:clikt:3.4.0")
+  implementation("dev.mobile:dadb:0.0.10")
+  implementation("com.github.ajalt.clikt:clikt:3.5.0")
   testImplementation(kotlin("test"))
 }
 
